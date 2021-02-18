29d9470b624862bd1d0ee7c957298034da57be42
lmodenez <lmodenez98@gmail.com>
2021-02-18 20:15:14 -0300

    finish frontend proffy

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+import './styles.css'
+
+
+
+
+function TeacherForm () {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader 
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo e preencher esse formulário de inscrição"  
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="WhatsApp"/>
+          <Textarea name="bio" label="Biografia" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Select 
+            name="subject" 
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação física', label: 'Educação física' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografica', label: 'Geografica' },
+              { value: 'História', label: 'História' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
+          <Input name="cost" label="Custo da sua hora por aula"/>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante ! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
+    </div>
+  );
+}
+
+export default TeacherForm;
